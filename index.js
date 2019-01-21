c1dc67a538025fc446fa0b1c293fedb4cb8e076e
SpaceboyRoss01 <spaceboyross@yandex.com>
2019-01-21 14:15:48 -0800

    Updated for inital release

--- index.js
+++ index.js
@@ -10,33 +10,68 @@ const register = (core, args, options, metadata) => {
   const _ = translatable(require('./locales.js'));
   let elem = document.createElement('div');
   elem.classList.add('game-root');
-  let store = new SimpleTetris.createStore();
+  let store = null;
+  let intv = null;
+  var blocksFromPreviousFrame;
   const render = () => {
-    let matrixBlocksToRender = SimpleTetris.matrixBlocksToRenderSelector(store.getState());
+    let blocks = SimpleTetris.matrixBlocksToRenderSelector(store.getState());
+    let domElement;
     let html = '';
-    for (let matrixBlockToRender of matrixBlocksToRender) html += matrixBlockToRender ? '<div class="filled"></div>' : '<div></div>';
-    elem.innerHTML = html;
+    let i;
+
+    if (!blocksFromPreviousFrame) {
+      for (i = 0; i < blocks.length; i++) {
+        html += blocks[i] ? '<div class="filled"></div>' : '<div></div>';
+      }
+      elem.innerHTML = html;
+    } else {
+      for (i = 0; i < blocks.length; i++) {
+        if (blocks[i] !== blocksFromPreviousFrame[i]) {
+          domElement = elem.querySelector('div:nth-child(' + (i + 1) + ')');
+          blocks[i]
+            ? domElement.classList.add('filled')
+            : domElement.classList.remove('filled');
+        }
+      }
+    }
+    blocksFromPreviousFrame = blocks;
   };
-  render();
+  const handleEvent = keyCode => {
+    SimpleTetris.gameLoopIteration(store, new Date().getTime(), keyCode);
+    requestAnimationFrame(render);
+  };
+  const run = () => {
+    store = new SimpleTetris.createStore({ time: new Date().getTime() });
+    intv = setInterval(handleEvent, SimpleTetris.TIME_STEP_DURATION);
+    requestAnimationFrame(render);
+  };
   proc.createWindow({
     id: 'TetrisWindow',
     title: _('WIN_TITLE'),
-    dimension: {width: 240, height: 480},
+    dimension: {width: 250, height: 500},
+    icon: proc.resource(proc.metadata.icon),
+    attributes: {
+      classNames: ['Window_Tetris'],
+      minDimension: {width: 250, height: 500},
+      maxDimension: {width: 250, height: 500},
+      maximizable: false
+    },
     position: 'center'
   }).on('destroy', () => {
     proc.destroy();
+    clearInterval(intv);
   }).on('keydown', ev => {
     let keyCode = 0;
     if (ev.keyCode == 37) keyCode = SimpleTetris.KEY_CODE_LEFT;
-    else if (ev.keyCode == 38 || ev.keyCode == 40) keyCode = SimpleTetris.KEY_CODE_ROTATE;
+    else if (ev.keyCode == 38) keyCode = SimpleTetris.KEY_CODE_ROTATE;
     else if (ev.keyCode == 39) keyCode = SimpleTetris.KEY_CODE_RIGHT;
-    else if (ev.keyCode == 32) keyCode = SimpleTetris.KEY_CODE_HARD_DROP;
-    SimpleTetris.gameLoopIteration(store, 0.0, keyCode);
-    render();
+    else if (ev.keyCode == 40) keyCode = SimpleTetris.KEY_CODE_HARD_DROP;
+    handleEvent(keyCode);
   }).render($content => {
     app({},{},(state,actions) => h(Box,{ grow: 1, padding: false },[
       h('div', { oncreate: el => el.appendChild(elem) }, [])
     ]), $content);
+    run();
   });
   return proc;
 };
